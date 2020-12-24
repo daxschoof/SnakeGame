e7f3c97318831e3fcc0152f847f198bec947130b
dlschoof <55069309+dlschoof@users.noreply.github.com>
2020-12-24 14:48:05 -0500

    Added Fruit and implemented Grid

--- snake.py
+++ snake.py
@@ -1,13 +1,50 @@
 import pygame
+import sys
+import random
+from pygame.math import Vector2
+
+# Constants for colors, cell size and number
+blue = (0, 0, 255)
+red = (255, 0, 0)
+white = (255, 255, 255)
+black = (0, 0, 0)
+cell_size = 40
+cell_number = 20
+
+# Initializing pygame and naming display
 pygame.init()
-dis=pygame.display.set_mode((400, 300))
 pygame.display.set_caption("Snake")
-pygame.display.update()
-game_over = False
 
-while not game_over:
+# Setting up clock and screen constants
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+
+# Classes for players
+class Fruit:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_obj(self):
+        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
+        pygame.draw.rect(screen, black, fruit_rect)
+
+fruit = Fruit()
+
+# Main game loop
+while True:
+    # Checking for events
     for event in pygame.event.get():
-        print(event)
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
 
-pygame.quit()
-quit()
+    # Setting background color
+    screen.fill((89, 212, 154))
+
+    fruit.draw_obj()
+
+    # Updating display and setting frame rate
+    pygame.display.update()
+    clock.tick(60)
